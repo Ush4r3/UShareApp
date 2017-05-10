2f7edfd552a6b505074ac16b03cdc145b03c55e9
Carlos Nunez <litos125@hotmail.com>
2017-05-09 20:17:26 -0500

    Add Splash - Main

--- app/src/main/java/com/ushare/usharemobile/SplashActivity.java
+++ app/src/main/java/com/ushare/usharemobile/SplashActivity.java
@@ -1,13 +1,33 @@
 package com.ushare.usharemobile;
 
+import android.content.Intent;
+import android.os.HandlerThread;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity implements Runnable {
+
+    Thread thread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        thread  = new Thread(this);
+        thread.start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            thread.sleep(3000);
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
